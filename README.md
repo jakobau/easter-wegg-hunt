2d582ed709a25b19a0f960b1aafdd6473c109b6b
jakobau <30506552+jakobau@users.noreply.github.com>
2020-04-05 21:36:27 -1000

    created node backend and react frontend simple socket.io application

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # easter-wegg-hunt
 Online easter egg hunt website platform for people and families that are social distancing! Find all the eggs in popular websites by solving riddles, puzzles, math problems, matching memes, and tapping randomly.
+
+https://www.valentinog.com/blog/socket-react/
---
+++ easter-wegg-hunt-server/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+
+//server on port 4001
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+var count = 0;
+const getApiAndEmit = async socket => {
+  try {
+    count = count + 1;
+    socket.emit("FromAPI", count); // Emitting a new message. It will be consumed by the client
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
+
+//handle multiple users
+let interval;
+
+io.on("connection", socket => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 10000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
