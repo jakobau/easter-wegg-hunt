ef712b1ed3ae6321ade4125472949a5e62dad3dc
Jakob Au <jakeau00@gmail.com>
2021-07-23 17:28:19 -1000

    add alert for secure url

--- public/js/init.js
+++ public/js/init.js
@@ -2,3 +2,10 @@ function hostGame(){
   var gamePin = Math.floor(Math.random()*90000) + 10000; //new pin for game
   window.location.href = "/create/?id="+gamePin;
 }
+
+var x = document.getElementById("message");
+if(window.location.href.indexOf("egghunt.jakobau.me") > -1){ 
+  x.style.display = "block";
+} else {
+  x.style.display = "none";
+}
